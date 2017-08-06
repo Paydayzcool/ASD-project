a7c41a067660ebbc3957cfba76eab836a7bfe951
Paydayzcool <30514471+Paydayzcool@users.noreply.github.com>
2017-08-06 11:24:15 +0800

    Building Good Mathematicians Project v0.0.1
    This only has the login screen

---
+++ Building Good Mathematicians/Building Good Mathematicians/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Building Good Mathematicians
+//
+//  Created by Eu Guan Koh on 6/8/17.
+//  Copyright © 2017 sqrt(i). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var usernameTextField: UITextField!
+
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
