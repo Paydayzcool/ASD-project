a7ecd7ace3b527fb23a67c272fcefcecadfc90fa
CHeyning <1083698@student.ccgs.wa.edu.au>
2017-08-08 21:15:07 +0800

    IT WORKS!!!!!!!
    We do still need to format the output and get rid of edge cases...

--- challenge.py
+++ challenge.py
@@ -1,30 +1,11 @@
-file = [i.strip().split() for i in open("results.txt").readlines()][1:]
+#making it non IT teacher proof
+try:
+    file = [i.strip().split() for i in open("results.txt").readlines()][1:]
+except:
+    print("There doesn't seem to be a results.txt document in the same file as this program. Make sure that the results are named results.txt with that spelling and .txt extension at the end as it needs to be a txt file for the program to work, an error will now occur and stop the program")
+
 #exclude 1st line because its just what the individual numbers mean
 #Event No, Age Level, Gender, Event Name, Place, StuCode, Competitor_First, Competitor_Last, Team/House, Performance, Points
-print('working...')
-'''#Discus function. Returns a sorted list of entries
-def discus(f):
-    array = []
-    for j in f:
-        # NOTE: Some discus entry lengths are larger than 11 because of the Student Codes - They can have spaces.
-        # Callum why does that matter with the way that you're doing it? Also we can just ignore any disqualified students, they wont be getting any points.
-        if len(j) >= 11:
-            array.append((float(j[-2]),float(j[-1]),j[-5]+" "+j[-4]))
-        else:
-            array.append((float(j[-1]),"DSQ",j[-4]+" "+j[-3]))
-    return sorted(array)[::-1]
-#High Jump function. Returns a sorted list of entries DOESN'T WORK YET
-def high_jump(f):
-    array = []
-    for i in f:
-        if len(i) >= 12:
-            array.append((float(i[-2]),float(i[-1]),i[-5]+" "+i[-4]))
-        else:
-            array.append((float(i[-1]),"DSQ",i[-4]+" "+i[-3]))
-    return sorted(array)
-        
-#Ok Coen, I've got Discus "Sorted"
-'''
 def isDist(num):
     try:
         float(num)
@@ -44,7 +25,7 @@ def high(f):
     for i in f:
         #if someone is disqualified it'll be their house which wont be a digit, int in output is place
         if i[-2][0].isdigit():
-            array.append((float(i[-2]),i[-7],i[-5]+" "+i[-4]))
+            array.append((float(i[-2]),i[-7],i[-5]+" "+i[-4], i[-3]))
             
     #if theres only one division we'll need it sorted properly
     return sorted(array)[::-1]
@@ -55,7 +36,7 @@ def low(f):
     for i in f:
         #if someone is disqualified it'll be their house which isnt an int, int in output is place
         if i[-2][0].isdigit() and i[-6][-2].isdigit():
-            array.append((timeToFloat(i[-2]),i[-7],i[-5]+" "+i[-4]))
+            array.append((timeToFloat(i[-2]),i[-7],i[-5]+" "+i[-4], i[-3]))
             
     array.sort()
     return array
@@ -94,48 +75,20 @@ while i < l:
             results[age_level+event] = sorted(results[age_level+event])
         else:
             results[age_level+event] = low(array)   
-        
-    '''
-    if event.lower() == "discus":
-        if age_level+event in results.keys():
-            results[age_level+event].extend(discus(array))
-            results[age_level+event] = sorted(results[age_level+event])[::-1]
-        else:
-            results[age_level+event] = discus(array)
-            
-    elif event.lower() == "high":
-        if age_level+event in results.keys():
-            results[age_level+event].extend(high_jump(array))
-            results[age_level+event] = sorted(results[age_level+event])[::-1]
-        else:
-            results[age_level+event] = high_jump(array)
-            
-    elif event.lower() == "triple":
-        if age_level+event in results.keys():
-            results[age_level+event].extend(triple_jump(array))
-            results[age_level+event] = sorted(results[age_level + event])[::-1]
-        else:
-            results[age_level+event] = triple_jump(array)
-    '''
 
     number += 1
     array = []
 
 class student:
-    def __init__(self, name, event, points):
+    def __init__(self, name, event, points, house):
         self.name = name
         self.events = [event]
         self.points = points
+        self.house = house
     def addEvent(self, event):
         self.events.extend(event)
     def addPoints(self, points):
         self.points+=points
-    def getPoints(self):
-        return(self.points)
-    def getName(self):
-        return(self.name)
-    def getEvents(self):
-        return(self.events)
 #all in the same dictionary makes it easy to put in
 students = {'U/13':{}, 'U/14':{}, 'U/15':{}, 'U/16':{}, 'U/17':{}, 'U/21':{}}
 #element is key and value is what value it stores, also this is how to loop over a dictionary if you want the values stored as well, otherwise you just go over the keys
@@ -143,35 +96,68 @@ for element, value in results.items():
     #element is key and value is definition
     #I test if the student has been entered already
     #The dictionary has seprate age groups which contain the students which are just dictionaries with their class in it
-    #value = performance,place, name for the smaller indexes
+    #value = performance,place, name, house for the smaller indexes
     #in the big mess of indexes element[0:4] is the age group eg U/13 which is defined in the dictionary, value is one of dictionary definitions of the ordered pooled events and element[4:] is the event name.
-    #WIP
     if value[0][2] in students[element[0:4]].keys():
-        students[element[0:4]][value[0][2]].addEvent((value[0][1],'place in',element[4:]))
+        students[element[0:4]][value[0][2]].addEvent([value[0][1],'place in',element[4:]])
         students[element[0:4]][value[0][2]].addPoints(10)
     else:
-        students[element[0:4]][value[0][2]] = student(value[0][2], (value[0][1],'place in',element[4:]), 10)
+        students[element[0:4]][value[0][2]] = student(value[0][2], [value[0][1],'place in',element[4:]], 10, value[0][3])
 
     if value[1][2] in students[element[0:4]].keys():
-        students[element[0:4]][value[1][2]].addEvent((value[1][1],'place in',element[4:]))
+        students[element[0:4]][value[1][2]].addEvent([value[1][1],'place in',element[4:]])
         students[element[0:4]][value[1][2]].addPoints(7)
     else:
-        students[element[0:4]][value[1][2]] = student(value[1][2], (value[1][1],'place in',element[4:]), 7)
+        students[element[0:4]][value[1][2]] = student(value[1][2], [value[1][1],'place in',element[4:]], 7, value[1][3])
         
     if value[2][2] in students[element[0:4]].keys():
-        students[element[0:4]][value[2][2]].addEvent((value[2][1],'place in',element[4:]))
+        students[element[0:4]][value[2][2]].addEvent([value[2][1],'place in',element[4:]])
         students[element[0:4]][value[2][2]].addPoints(5)
     else:
-        students[element[0:4]][value[2][2]] = student(value[2][2], (value[2][1],'place in',element[4:]), 5)
+        students[element[0:4]][value[2][2]] = student(value[2][2], [value[2][1],'place in',element[4:]], 5, value[2][3])
         
     if value[3][2] in students[element[0:4]].keys():
-        students[element[0:4]][value[3][2]].addEvent((value[3][1],'place in',element[4:]))
+        students[element[0:4]][value[3][2]].addEvent([value[3][1],'place in',element[4:]])
         students[element[0:4]][value[3][2]].addPoints(3)
     else:
-        students[element[0:4]][value[3][2]] = student(value[3][2], (value[3][1],'place in',element[4:]), 3)
+        students[element[0:4]][value[3][2]] = student(value[3][2], [value[3][1],'place in',element[4:]], 3, value[3][3])
         
     if value[4][2] in students[element[0:4]].keys():
-        students[element[0:4]][value[4][2]].addEvent((value[4][1],'place in',element[4:]))
+        students[element[0:4]][value[4][2]].addEvent([value[4][1],'place in',element[4:]])
         students[element[0:4]][value[4][2]].addPoints(1)
     else:
-        students[element[0:4]][value[4][2]] = student(value[0][2], (value[4][1],'place in',element[4:]), 1)
+        students[element[0:4]][value[4][2]] = student(value[4][2], [value[4][1],'place in',element[4:]], 1, value[4][3])
+    #assigning the rest of the events that don't give points, we still need them
+    for i in range(5,len(value)):
+        if value[i][2] in students[element[0:4]].keys():
+            students[element[0:4]][value[i][2]].addEvent([value[i][1], 'place in',element[4:]])
+        else:
+            students[element[0:4]][value[i][2]] = student(value[i][2], [value[i][1],'place in',element[4:]], 0, value[i][3])
+
+u13 = []
+u14 = []
+u15 = []
+u16 = []
+u17 = []
+u21 = []
+def getTop(ageGroup):
+    output = []
+    for name, data in students[ageGroup].items():
+        output.append([data.points, data.name])
+    output.sort()
+    #sort puts it into an ascending order
+    output = output[::-1]
+    print('Age Group =', ageGroup)
+    print(output[0][1],'House='+ students[ageGroup][output[0][1]].house+',', str(output[0][0]), "Points")
+    print(students[ageGroup][output[0][1]].events)
+    #not inclusive of final element
+    for i in range(1,5):
+        print(output[i][1]+',', students[ageGroup][output[i][1]].house+',', output[i][0], "Points")
+        
+    print(30*'=')
+u13 = getTop('U/13')
+u14 = getTop('U/14')
+u15 = getTop('U/15')
+u16 = getTop('U/16')
+u17 = getTop('U/17')
+u21 = getTop('U/21')
