9828f5fc75c5ef22eab66493e4f23b65f9d78459
CHeyning <1083698@student.ccgs.wa.edu.au>
2017-08-29 10:54:31 +0800

    Add files via upload

--- databaseCode.py
+++ databaseCode.py
@@ -3,17 +3,18 @@ con = connect('project.db')
 cur = con.cursor()
 
 def newStudent(ID,fname,lname,classes):
-    try:
-        cur.execute('INSERT INTO Students VALUES(?,?,?)',(ID,fname,lname))
-
-        classes = classes.split()
-        for i in classes:
-            cur.execute('INSERT INTO ReferenceS VALUES(?,?)', (ID, i))
-    
-        con.commit()                        
-    except:
+    #try:
+    cur.execute('INSERT INTO Students VALUES(?,?,?)',(ID,fname,lname))
+    classes = classes.split()
+    for i in classes:
+        print(ID,i)
+#       cur.execute('INSERT INTO ReferenceS (Student, Class) VALUES (?,?)',(ID, i))
+        print('INSERT INTO ReferenceS (Student, Class) VALUES (' + str(ID) + ',"' + i + '")')
+        cur.execute('INSERT INTO ReferenceS VALUES (' + str(ID) + ',"' + i + '")')
+    con.commit()                        
+    """except:
         print('error newStudent')
-        con.rollback()
+        con.rollback()"""
 
 
 def newTeacher(ID, fname, lname, classes):
@@ -30,10 +31,17 @@ def newTeacher(ID, fname, lname, classes):
 
 def newTask(Class,questions,answers,name):
     try:
-        cur.execute('INSERT INTO Tasks VALUES(?,?,?,?)',(Class,name,questions,answers))
+        cur.execute('SELECT TaskID FROM TASKS WHERE Questions =',questions,'AND Answers =',answers)
+        data = cur.fetchall()
+        if data.len() == 0:
+            cur.execute('INSERT INTO Tasks VALUES(?,?,?,?)',(questions,answers,name))
+        
         cur.execute('SELECT Student FROM ReferenceS WHERE Class =',Class)
         data = cur.fetchall()
-        #Need to get data
+        print(data)
+        cur.execute('SELECT TaskID FROM Tasks WHERE Questions =',questions,'AND', 'Answers =',answers)
+        ID = cur.fetchall()
+        print(ID)
         for student in data:
             cur.execute('INSERT INTO Completion VALUES(?,?,?)',(student,ID,'N'))
             
@@ -41,6 +49,7 @@ def newTask(Class,questions,answers,name):
     except:
         print('error newTask')
         con.rollback()
-    
+
+newStudent(1083698, 'Coen', 'Heyning', '10MSA02') 
 con.commit()
 con.close()
