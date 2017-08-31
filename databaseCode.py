0c4bed93970d71b871aaadb8a587f27024cec77a
CHeyning <1083698@student.ccgs.wa.edu.au>
2017-08-31 18:45:40 +0800

    Add files via upload

--- databaseCode.py
+++ databaseCode.py
@@ -50,6 +50,35 @@ def newTask(Class,questions,answers,name):
         print('error newTask')
         con.rollback()
 
-newStudent(1083698, 'Coen', 'Heyning', '10MSA02') 
-con.commit()
+def getTasks(ID, Class):
+    try:
+        cur.execute('SELECT ID FROM Completion WHERE student =',ID)
+        taskIDs = cur.fetchall()
+        tasks = []
+        for task in taskIDs:
+            cur.execute('SELECT Questions,Answers FROM Tasks WHERE ID =',task)
+            tasks.append(cur.fetchall())
+        return(tasks)
+    except:
+        print('error getTasks')
+        con.rollback()
+
+def getStudents(Class):
+    try:
+        cur.execute('SELECT Student FROM ReferenceS WHERE Class =',Class)
+        students = cur.fetchall()
+        return(students)
+    except:
+        print('Error getStudents')
+        con.rollback()
+
+def complete(student,task):
+    try:
+        cur.execute('Completion SET Complete = "Y" WHERE Student =', student,'AND Task =',task)
+        con.commit()
+    except:
+        print('error complete')
+        con.rollback()
+'''newStudent(1083698, 'Coen', 'Heyning', '10MSA02') 
+con.commit()'''
 con.close()
